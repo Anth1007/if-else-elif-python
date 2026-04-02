5d07d6cd45cf5c51629d0e8e00320e19f98c0de2
Anthony Gomes <anthonygabreu@icloud.com>
2026-04-02 01:04:05 -0300

    Update classificador_de_idades.py

--- classificador_de_idades.py
+++ classificador_de_idades.py
@@ -6,14 +6,14 @@ idade = int(input("insira sua idade: "))
  print(f"Olá, {nome}!") 
 
 #utilizado if,elif, else para classificar o usuario de acordo com sua idade
- if idade <= 11:
+if idade <= 11:
      print("Vocë é uma criança, sem responsabilidades, vá se divertir, Brinque, aprenda, corra")
- elif idade <= 17:
+elif idade <= 17:
      print("Vocë é um adolescente, aproveite essa fase da vida Estude, se desenvolva, socialize")
- elif idade <= 64:
-     print("Vocë é adulto, Trabalhe, tome decisoës, viaje, seja independente")
- else:
-     print("Vocë é idoso, Descanse, curta o lazer, compartilhe a experiência de vida")
+elif idade <= 64:
+     print("Vocë é um adulto, Trabalhe, tome decisoës, viaje, seja independente")
+else:
+     print("Vocë é um idoso, Descanse, curta o lazer, compartilhe a experiência de vida")
     
 
 
