db68e9429ca5592b4fdc51ad59390dd09afd9f4c
Anthony Gomes <anthonygabreu@icloud.com>
2026-04-02 00:48:02 -0300

    Add files via upload
    Adiciona projeto de classificação de idade.

---
+++ classificador_de_idades.py
@@ -0,0 +1,20 @@
+# nome = input("Insira seu nome: ")
+# idade = int(input("insira sua idade: "))
+ 
+# print(nome)
+
+# if idade <= 11:
+#     print("Vocë é uma criança, sem responsabilidades, vá se divertir, Brinque, aprenda, corra")
+# elif idade <= 17:
+#     print("Vocë é um adolescente, aproveite essa fase da vida Estude, se desenvolva, socialize")
+# elif idade <= 64:
+#     print("Vocë é adulto, Trabalhe, tome decisoës, viaje, seja independente")
+# else:
+#     print("Vocë é idoso, Descanse, curta o lazer, compartilhe a experiência de vida")
+
+    
+
+
+
+
+   
